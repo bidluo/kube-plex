b632217a9d4dfafb90ce56216a7f17295cd5d730
David Liaw <david@liaw.me>
2022-02-23 15:54:59 +1100

    Mount the transcode dierector at /tmp

--- main.go
+++ main.go
@@ -183,6 +183,10 @@ func generatePod(cwd string, env []string, args []string) *corev1.Pod {
 							Name:      "transcode",
 							MountPath: "/transcode",
 						},
+						{
+							Name:      "transcode",
+							MountPath: "/tmp",
+						},
 					},
 				},
 			},
